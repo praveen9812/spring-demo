b0cce17409dd664842db2a81b099f277d77d0f77
praveen9812 <praveen.himantha@gmail.com>
2021-07-25 19:27:25 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot/PraveenApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraveenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraveenApplication.class, args);
+	}
+
+}
